e91d5d9b39bf9539965cc6dee048295bbf6298df
msk <msk@huray.net>
2023-12-02 20:35:36 +0900

    Object Delegate

---
+++ UE5Part1/UnrealDelegate/Source/UnrealDelegate/CourseInfo.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "CourseInfo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALDELEGATE_API UCourseInfo : public UObject
+{
+	GENERATED_BODY()
+};
