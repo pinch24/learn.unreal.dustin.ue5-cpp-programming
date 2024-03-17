60c1dd1691b0766a5a60da62a7c77b7cbfaac08e
pinch24 <pinch24@gmail.com>
2024-03-17 20:33:08 +0900

    TMap, TMultiMap

--- UE5Part1/UnrealContainer/Source/UnrealContainer/MyGameInstance.h
+++ UE5Part1/UnrealContainer/Source/UnrealContainer/MyGameInstance.h
@@ -6,9 +6,36 @@
 #include "Engine/GameInstance.h"
 #include "MyGameInstance.generated.h"
 
-/**
- * 
- */
+USTRUCT()
+struct FStudentData
+{
+	GENERATED_BODY()
+
+	FStudentData()
+	{
+		Name = TEXT("Mason");
+		Order = -1;
+	}
+
+	FStudentData(FString InName, int32 InOrder) : Name(InName), Order(InOrder) {}
+
+	bool operator==(const FStudentData& InOther) const
+	{
+		return Order == InOther.Order;
+	}
+
+	friend FORCEINLINE uint32 GetTypeHash(const FStudentData& InStudentData)
+	{
+		return GetTypeHash(InStudentData.Order);
+	}
+
+	UPROPERTY()
+	FString Name;
+
+	UPROPERTY()
+	int32 Order;
+};
+
 UCLASS()
 class UNREALCONTAINER_API UMyGameInstance : public UGameInstance
 {
@@ -16,4 +43,12 @@ class UNREALCONTAINER_API UMyGameInstance : public UGameInstance
 
 public:
 	virtual void Init() override;
+
+private:
+	TArray<FStudentData> StudentsData;
+
+	UPROPERTY()
+	TArray<TObjectPtr<class UStudent>> Students;
+	
+	TMap<int32, FString> StudentsMap;
 };
