84a42f63c2d167979e31bdb78fc3a14d4beb3130
mk <pinch24@gmail.com>
2024-05-25 13:55:08 +0900

    Unreal Serialization

---
+++ UE5Part1/UnrealSerialization/Source/UnrealSerialization/MyGameInstance.cpp
@@ -0,0 +1,115 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameInstance.h"
+#include "JsonObjectConverter.h"
+#include "Student.h"
+
+void PrintStudentInfo(const UStudent* InStudent, const FString& InTag)
+{
+	UE_LOG(LogTemp, Warning, TEXT("[%s] Name: %s, Seq: %d"), *InTag, *InStudent->GetName(), InStudent->GetOrder());
+}
+
+UMyGameInstance::UMyGameInstance()
+{
+}
+
+void UMyGameInstance::Init()
+{
+	Super::Init();
+
+	FStudentData RawDataSrc(16, TEXT("Bravo"));
+
+	const FString SavedDir = FPaths::Combine(FPlatformMisc::ProjectDir(), TEXT("Saved"));
+	UE_LOG(LogTemp, Warning, TEXT("Saved File Directory: %s"), *SavedDir);
+
+	{
+		const FString RawDataFileName(TEXT("RawData.bin"));
+		FString RawDataAbsolutePath = FPaths::Combine(*SavedDir, *RawDataFileName);
+		UE_LOG(LogTemp, Warning, TEXT("Saved File Path: %s"), *RawDataAbsolutePath);
+		FPaths::MakeStandardFilename(RawDataAbsolutePath);
+		UE_LOG(LogTemp, Warning, TEXT("Edited File Path: %s"), *RawDataAbsolutePath);
+
+		FArchive* RawFileWriterAr = IFileManager::Get().CreateFileWriter(*RawDataAbsolutePath);
+		if (RawFileWriterAr != nullptr)
+		{
+			*RawFileWriterAr << RawDataSrc;
+			RawFileWriterAr->Close();
+			delete RawFileWriterAr;
+			RawFileWriterAr = nullptr;
+		}
+
+		FStudentData RawDataDest;
+		FArchive* RawFileReaderAr = IFileManager::Get().CreateFileReader(*RawDataAbsolutePath);
+		if (RawFileReaderAr != nullptr)
+		{
+			*RawFileReaderAr << RawDataDest;
+			RawFileReaderAr->Close();
+			delete RawFileReaderAr;
+			RawFileReaderAr = nullptr;
+
+			UE_LOG(LogTemp, Warning, TEXT("[RawData] Name: %s, Seq: %d"), *RawDataDest.Name, RawDataDest.Order);
+		}
+	}
+
+	StudentSrc = NewObject<UStudent>();
+	StudentSrc->SetName(TEXT("Delta"));
+	StudentSrc->SetOrder(59);
+
+	{
+		const FString ObjectDataFileName(TEXT("ObjectData.bin"));
+		FString ObjectDataAbsolutePath = FPaths::Combine(*SavedDir, *ObjectDataFileName);
+		FPaths::MakeStandardFilename(ObjectDataAbsolutePath);
+
+		TArray<uint8> BufferArray;
+		FMemoryWriter MemoryWriterAr(BufferArray);
+		StudentSrc->Serialize(MemoryWriterAr);
+		
+		if (TUniquePtr<FArchive> FileWriterAr = TUniquePtr<FArchive>(IFileManager::Get().CreateFileWriter(*ObjectDataAbsolutePath)))
+		{
+			*FileWriterAr << BufferArray;
+			FileWriterAr->Close();
+		}
+		
+		TArray<uint8> BufferArrayFromFile;
+		if (TUniquePtr<FArchive> FileReaderAr = TUniquePtr<FArchive>(IFileManager::Get().CreateFileReader(*ObjectDataAbsolutePath)))
+		{
+			*FileReaderAr << BufferArrayFromFile;
+			FileReaderAr->Close();
+		}
+		
+		FMemoryReader MemoryReaderAr(BufferArrayFromFile);
+		UStudent* StudentDest = NewObject<UStudent>();
+		StudentDest->Serialize(MemoryReaderAr);
+		PrintStudentInfo(StudentDest, TEXT("ObjectData"));
+	}
+
+	{
+		const FString JsonDataFileName(TEXT("StudentJsonData.txt"));
+		FString JsonDataAbsolutePath = FPaths::Combine(*SavedDir, *JsonDataFileName);
+		FPaths::MakeStandardFilename(JsonDataAbsolutePath);
+
+		TSharedRef<FJsonObject> JsonObjectSrc = MakeShared<FJsonObject>();
+		FJsonObjectConverter::UStructToJsonObject(StudentSrc->GetClass(), StudentSrc, JsonObjectSrc);
+
+		FString JsonOutString;
+		TSharedRef<TJsonWriter<TCHAR>> JsonWriterAr = TJsonWriterFactory<TCHAR>::Create(&JsonOutString);
+		if (FJsonSerializer::Serialize(JsonObjectSrc, JsonWriterAr))
+		{
+			FFileHelper::SaveStringToFile(JsonOutString, *JsonDataAbsolutePath);
+		}
+
+		FString JsonInString;
+		FFileHelper::LoadFileToString(JsonInString, *JsonDataAbsolutePath);
+		TSharedRef<TJsonReader<TCHAR>> JsonReaderAr = TJsonReaderFactory<TCHAR>::Create(JsonInString);
+		TSharedPtr<FJsonObject> JsonObjectDest;
+		if (FJsonSerializer::Deserialize(JsonReaderAr, JsonObjectDest))
+		{
+			UStudent* JsonStudentDest = NewObject<UStudent>();
+			if (FJsonObjectConverter::JsonObjectToUStruct(JsonObjectDest.ToSharedRef(), JsonStudentDest->GetClass(), JsonStudentDest))
+			{
+				PrintStudentInfo(JsonStudentDest, TEXT("JsonData"));
+			}
+		}
+	}
+}
