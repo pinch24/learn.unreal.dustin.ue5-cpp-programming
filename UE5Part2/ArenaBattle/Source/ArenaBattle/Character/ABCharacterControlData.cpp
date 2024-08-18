ec77cbe04431f0e61c9dad3ca73590796cf8e14d
mk <pinch24@gmail.com>
2024-08-18 23:40:34 +0900

    AB - Character Control

---
+++ UE5Part2/ArenaBattle/Source/ArenaBattle/Character/ABCharacterControlData.cpp
@@ -0,0 +1,2 @@
+#include "Character/ABCharacterControlData.h"
+
---
+++ UE5Part2/ArenaBattle/Source/ArenaBattle/Character/ABCharacterControlData.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ABCharacterControlData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABATTLE_API UABCharacterControlData : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+};
