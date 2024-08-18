c0e217b7d7ea246832b27bfa77dc83d8ed7a2b4b
mk <pinch24@gmail.com>
2024-08-18 22:45:59 +0900

    AB - Comment

--- UE5Part2/ArenaBattle/Source/ArenaBattle/Character/ABCharacterBase.cpp
+++ UE5Part2/ArenaBattle/Source/ArenaBattle/Character/ABCharacterBase.cpp
@@ -1,7 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-
 #include "Character/ABCharacterBase.h"
 #include "Components/CapsuleComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
