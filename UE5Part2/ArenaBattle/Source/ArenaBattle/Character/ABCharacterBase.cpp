a9bd0b7a2d67337c94fb4a97535bcc5258a8bb52
mk <pinch24@gmail.com>
2024-08-18 13:22:14 +0900

    AB - Input

--- UE5Part2/ArenaBattle/Source/ArenaBattle/Character/ABCharacterBase.cpp
+++ UE5Part2/ArenaBattle/Source/ArenaBattle/Character/ABCharacterBase.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+
 #include "Character/ABCharacterBase.h"
 #include "Components/CapsuleComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
--- UE5Part2/ArenaBattle/Source/ArenaBattle/Character/ABCharacterPlayer.h
+++ UE5Part2/ArenaBattle/Source/ArenaBattle/Character/ABCharacterPlayer.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputActionValue.h"
 #include "Character/ABCharacterBase.h"
 #include "ABCharacterPlayer.generated.h"
 
@@ -15,12 +16,35 @@ class ARENABATTLE_API AABCharacterPlayer : public AABCharacterBase
 	GENERATED_BODY()
 
 public:
+	// Constructor
 	AABCharacterPlayer();
 
 protected:
+    virtual void BeginPlay() override;
+
+	// Camera Section
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = CAMERA, Meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<class USpringArmComponent> CameraBoom;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, Meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<class UCameraComponent> FollowCamera;
+
+	// Input Section
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputMappingContext> DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> JumpAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> LookAction;
+	
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+
+public:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 };
