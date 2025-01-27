64d6b752445cb570a9a49fc8bd6c0df0fff6cd03
mk <pinch24@gmail.com>
2025-01-27 23:23:31 +0900

    AB - Local/Remote Log

--- UE5Part3/ArenaBattle5.5/Source/ArenaBattle/ArenaBattle.h
+++ UE5Part3/ArenaBattle5.5/Source/ArenaBattle/ArenaBattle.h
@@ -2,12 +2,14 @@
 
 #include "CoreMinimal.h"
 
+#define LOG_LOCALROLEINFO *(UEnum::GetValueAsString(TEXT("Engine.ENetRole"), GetLocalRole()))
+#define LOG_REMOTEROLEINFO *(UEnum::GetValueAsString(TEXT("Engine.ENetRole"), GetRemoteRole()))
 #define LOG_NETMODEINFO \
     ((GetNetMode() == NM_Client) ? *FString::Printf(TEXT("CLIENT_%d"), static_cast<int32>(GPlayInEditorID)) : \
     ((GetNetMode() == NM_Standalone) ? TEXT("STANDALONE") : \
     ((GetNetMode() == NM_ListenServer) ? TEXT("LISTEN_SERVER") : TEXT("DEDICATED_SERVER"))))
 #define LOG_CALLINFO ANSI_TO_TCHAR(__FUNCTION__)
-#define AB_LOG(LogCat, Verbosity, Format, ...) UE_LOG(LogCat, Verbosity, TEXT("[%s] %s %s"), LOG_NETMODEINFO, LOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
+#define AB_LOG(LogCat, Verbosity, Format, ...) UE_LOG(LogCat, Verbosity, TEXT("[%s][%s/%s] %s %s"), LOG_NETMODEINFO, LOG_LOCALROLEINFO, LOG_REMOTEROLEINFO,LOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
 
 DECLARE_LOG_CATEGORY_EXTERN(LogABNetwork, Log, All);
 
